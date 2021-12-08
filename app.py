337fd5b614b62f3d81334a5669f49fd6457fef07
Fernando <fernandorodriguezdata@hotmail.com>
2021-12-08 13:37:11 -0300

    Create CRUD

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request
 from flaskext.mysql import MySQL
 from pymysql import cursors
+from datetime import datetime
 
 
 
@@ -39,8 +40,17 @@ def storage():
     _correo = request.form['txtCorreo']
     _foto   = request.files['txtFoto']
 
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    nuevoNombreFoto = tiempo + _foto.filename
+
+    #Fix me, si no sube foto, rompe todo.
+    if _foto.filename!="":
+        _foto.save("uploads/"+nuevoNombreFoto)
+
     sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-    datos = [_nombre,_correo,_foto]
+    datos = [_nombre,_correo,nuevoNombreFoto]
+
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql,datos)
